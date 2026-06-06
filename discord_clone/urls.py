054e8794853d4ad05cc22c2af99bd84a8a949d57
Dawid <dawidskrobol4@gmail.com>
2026-06-06 21:20:02 +0200

    naprawa zdjec

--- discord_clone/urls.py
+++ discord_clone/urls.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path, include, re_path 
 from django.conf import settings
 from django.conf.urls.static import static
-
-def home(request):
-    if request.user.is_authenticated:
-        return redirect('/chat/general/') 
-    return redirect('login')
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    path('', include('users.urls')),
     path('chat/', include('chat.urls')),
-    path('', home, name='home'),
+]
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
 ]
 
 if settings.DEBUG:
