84526914def2290067a6767ed3c8dcf876f312e4
Dawid <dawidskrobol4@gmail.com>
2026-06-06 21:30:30 +0200

    aa

--- discord_clone/urls.py
+++ discord_clone/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include, re_path 
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static 
 from django.views.static import serve 
+from django.views.generic import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    path('', RedirectView.as_view(url='/chat/general/'), name='home'), 
+    
     path('', include('users.urls')),
     path('chat/', include('chat.urls')),
 ]
@@ -18,6 +22,7 @@ urlpatterns += [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 handler404 = 'users.views.custom_404'
     
